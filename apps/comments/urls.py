78ad3ac921408fcb3f1a0f36c03ba12549778d03
Pema Sherpa <pemasherpa@Pemas-MacBook-Air.local>
2026-06-30 10:54:17 +0545

    Used ModelViewSets instead of generics, added permissions.py file [Comments Part]

--- apps/comments/urls.py
+++ apps/comments/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from apps.comments.views import TaskCommentListCreateView, CommentDetailView
+from .views import CommentViewSet
 
 urlpatterns = [
-    path('projects/<int:pid>/tasks/<int:tid>/comments/', TaskCommentListCreateView.as_view(), name='task-comment-list-create'),
-    path('comments/<int:pk>/', CommentDetailView.as_view(), name='comment-detail'),
+    path('projects/<int:pid>/tasks/<int:tid>/comments/', CommentViewSet.as_view({
+        'get': 'list',
+        'post': 'create'
+    }), name='comment-list'),
+    
+    path('projects/<int:pid>/tasks/<int:tid>/comments/<int:pk>/', CommentViewSet.as_view({
+        'get': 'retrieve',
+        'put': 'update',
+        'patch': 'partial_update',
+        'delete': 'destroy'
+    }), name='comment-detail'),
 ]
