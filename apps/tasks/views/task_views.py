a5c802d1887ea54de2a9ff5ccfcd0f83f59d9027
Prasna Basnet <prasnabasnet18@gmail.com>
2026-07-06 10:26:19 +0545

    refactor(tasks): change the views from generics to modelviewset

--- apps/tasks/views/task_views.py
+++ apps/tasks/views/task_views.py
@@ -1,39 +1,18 @@
 from django.shortcuts import render
-
-# Create your views here.
-from rest_framework import generics, permissions
 from apps.tasks.models.task import Task
 from apps.tasks.serializers.task import TaskSerializer
+from rest_framework import viewsets, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 
 
-class TaskListCreateView(generics.ListCreateAPIView):
+class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['project', 'status', 'priority', 'assignee']
 
     def get_queryset(self):
-        qs = Task.objects.filter(project__members=self.request.user)
-        
-        project_id = self.request.query_params.get('project')
-        status = self.request.query_params.get('status')
-        priority = self.request.query_params.get('priority')
-        assignee = self.request.query_params.get('assignee')
-
-        if project_id:
-            qs = qs.filter(project_id=project_id)
-        if status:
-            qs = qs.filter(status=status)
-        if priority:
-            qs = qs.filter(priority=priority)
-        if assignee:
-            qs = qs.filter(assignee_id=assignee)
-
-        return qs.distinct()
+        return Task.objects.filter(created_by=self.request.user)
  
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
-
-
-class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializer
-    permission_classes = [permissions.IsAuthenticated]
