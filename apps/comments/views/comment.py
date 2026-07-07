ca0664ebd8ab259b6f857060a3017357ebc6d882
Pema Sherpa <ptssherpa5@gmail.com>
2026-07-07 10:29:51 +0545

    fixed inline errors

--- apps/comments/views/comment.py
+++ apps/comments/views/comment.py
@@ -1,6 +1,4 @@
-from rest_framework import viewsets, permissions, status
-from rest_framework.response import Response
-from rest_framework.exceptions import ValidationError
+from rest_framework import viewsets, permissions
 from django.contrib.contenttypes.models import ContentType
 
 from apps.comments.models import Comment
