7e3ea7643c4c4a9c0c658a81cc7015783b44319a
Prasna Basnet <prasnabasnet18@gmail.com>
2026-07-09 16:41:39 +0545

    chore(organization): add serializer for  organization model

---
+++ apps/organization/serializers/organization.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from apps.organization.models import Organization
+from apps.users.serializers import UserDetailSerializer
+from django.contrib.auth import get_user_model
+
+user = get_user_model()
+class OrganizationSerializer(serializers.ModelSerializer):
+    owner = UserDetailSerializer(read_only=True)
+    members = UserDetailSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Organization
+        fields = ['__all__']
+        read_only_fields = ['id', 'slug', 'created_at', 'updated_at']
+
+    def create(self, validated_data):
+        validated_data['owner'] = self.context['request'].user
+        return super().create(validated_data)
