e8ab5caafef42c5db49d834fb137cfaf645a9903
Pema <ptssherpa5@gmail.com>
2026-07-28 10:50:13 +0545

    feat: Implement base service and .execute for department

--- backend/apps/department/services.py
+++ backend/apps/department/services.py
@@ -1,48 +1,54 @@
 from rest_framework.exceptions import NotFound, PermissionDenied
+
+from apps.common.services import BaseService
 from apps.organization.models import Organization
 from apps.projects.serializers import ProjectSerializer
 
 
-class DepartmentService:
-    @staticmethod
-    def get_organization(pk, user):
+class BaseDepartmentService(BaseService):
+    def get_organization(self, pk, user):
         try:
             org = Organization.objects.get(pk=pk)
         except Organization.DoesNotExist:
-            raise NotFound("Organization not found.")
+            raise NotFound("Organization not found")
 
-        if not user.is_admin:
+        if not getattr(user, "is_admin", False):
             is_owner = org.owner == user
             is_member = org.memberships.filter(user=user).exists()
             if not (is_owner or is_member):
-                raise PermissionDenied("You are not a member of this organization.")
+                raise PermissionDenied("You are not a member of this organization")
+
         return org
 
-    @classmethod
-    def get_departments(cls, organization_id, user):
-        org = cls.get_organization(organization_id, user)
+
+class GetDepartmentService(BaseDepartmentService):
+    def process(self):
+        org = self.get_organization(self.organization_id, self.user)
         return org.departments.all()
 
-    @classmethod
-    def create_department(cls, organization_id, user, serializer):
-        org = cls.get_organization(organization_id, user)
-        return serializer.save(organization=org)
 
-    @staticmethod
-    def get_department_projects(department):
-        return department.projects.all()
+class CreateDepartmentService(BaseDepartmentService):
+    def process(self):
+        org = self.get_organization(self.organization_id, self.user)
+        return self.serializer.save(organizatoin=org)
 
-    @staticmethod
-    def create_project(department, user, data, request):
-        if not (user.is_admin or user.role == "PM"):
-            raise PermissionDenied("Only admins and project managers can create projects.")
 
-        data = data.copy()
+class GetDepartmentProjectService(BaseService):
+    def process(self):
+        return self.departments.projects.all()
+
+
+class CreateProjectService(BaseService):
+    def process(self):
+        user = self.user
+        department = self.department
+
+        if not (getattr(user, "is_admin", False) or getattr(user, "role", "") == "PM"):
+            raise PermissionDenied("You are not authorized to create project")
+
+        data = self.data.copy()
         data["department"] = department.pk
-        serializer = ProjectSerializer(
-            data=data,
-            context={"request": request}
-        )
+        serializer = ProjectSerializer(data=data, context={"request": self.request})
         serializer.is_valid(raise_exception=True)
         serializer.save(owner=user)
         return serializer.data
--- backend/apps/department/views/department.py
+++ backend/apps/department/views/department.py
@@ -1,14 +1,18 @@
-from apps import department
-from rest_framework import viewsets
+from drf_spectacular.utils import extend_schema
+from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from drf_spectacular.utils import extend_schema
 
 from apps.department.serializers import DepartmentSerializer
+from apps.department.services import (
+    CreateDepartmentService,
+    CreateProjectService,
+    GetDepartmentProjectService,
+    GetDepartmentService,
+)
 from apps.projects.serializers import ProjectSerializer
 from apps.users.permissions import IsAdminOrOrgOwner
-from apps.department.services import DepartmentService
 
 
 class DepartmentViewSet(viewsets.ModelViewSet):
@@ -20,10 +24,16 @@ class DepartmentViewSet(viewsets.ModelViewSet):
         return [IsAuthenticated()]
 
     def get_queryset(self):
-        return DepartmentService.get_departments(self.kwargs["oid"], self.request.user)
+        return GetDepartmentService.execute(
+            organization_id=self.kwargs["oid"], user=self.request.user
+        )
 
     def perform_create(self, serializer):
-        DepartmentService.create_department(self.kwargs["oid"], self.request.user, serializer)
+        return CreateDepartmentService.execute(
+            organization_id=self.kwargs["oid"],
+            user=self.request.user,
+            data=serializer.validated_data,
+        )
 
     @extend_schema(
         responses={200: ProjectSerializer(many=True)},
@@ -33,7 +43,9 @@ class DepartmentViewSet(viewsets.ModelViewSet):
     @action(detail=True, methods=["get"], url_path="projects")
     def list_projects(self, request, oid=None, pk=None):
         department = self.get_object()
-        projects = DepartmentService.get_department_projects(department)
+        projects = GetDepartmentProjectService.execute(
+            department=department, user=request.user
+        )
         serializer = ProjectSerializer(projects, many=True)
         return Response(serializer.data)
 
@@ -46,11 +58,8 @@ class DepartmentViewSet(viewsets.ModelViewSet):
     @action(detail=True, methods=["post"], url_path="projects")
     def create_project(self, request, oid=None, pk=None):
         department = self.get_object()
-        data = DepartmentService.create_project(
-            department=department,
-            user=request.user,
-            data=request.data,
-            request=request
+        project_data = CreateProjectService.execute(
+            department=department, user=request.user, data=request.data, request=request
         )
-        return Response(data, status=201)
 
+        return Response(project_data, status=status.HTTP_201_CREATED)
