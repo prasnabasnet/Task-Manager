715d1d15b1b6e7af3f59409792044e7373eddb60
Prasna Basnet <prasnabasnet18@gmail.com>
2026-07-28 11:19:59 +0545

    refactor(department): use DefaultRouter instead of manually mapping actions

--- apps/department/urls.py
+++ apps/department/urls.py
@@ -1,22 +1,12 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
 from apps.department.views import DepartmentViewSet
 
-dept_list = DepartmentViewSet.as_view({"get": "list", "post": "create"})
-dept_detail = DepartmentViewSet.as_view(
-    {"get": "retrieve", "patch": "partial_update", "put": "update", "delete": "destroy"}
-)
+router = DefaultRouter()
 
-dept_projects = DepartmentViewSet.as_view({"get": "projects"})
+router.register(r"", DepartmentViewSet, basename="department")
 
 urlpatterns = [
-    path("organizations/<int:oid>/departments/", dept_list, name="dept-list"),
-    path(
-        "organizations/<int:oid>/departments/<int:pk>/", dept_detail, name="dept-detail"
-    ),
-    path(
-        "organizations/<int:oid>/departments/<int:pk>/projects/",
-        dept_projects,
-        name="dept-project-list",
-    ),
+    path("", include(router.urls)),
 ]
